b34b470c2f0bb39e486c23c8fe0ea5e2afdbaf00
G2F1SH <136307552+G2F1SH@users.noreply.github.com>
2024-12-12 03:10:43 +0800

    Add files via upload

---
+++ shaders/libs/settings.glsl
@@ -0,0 +1,4 @@
+#define SUN_ROTATION  15.0 // [15.0 30.0 45.0 60.0 75.0]
+#define SHADOW_MAPRES  1024 // [1024 2048 4096]
+const float sunPathRotation = SUN_ROTATION;
+const int shadowMapResolution = SHADOW_MAPRES;
